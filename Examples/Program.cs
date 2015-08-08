2708059aad386bb21d062d8b7f46d2df5eb0b2c3
Josef Pihrt <josef.pihrt@outlook.com>
2015-08-08 13:15:35 +0200

    "Examples" project added

---
+++ Examples/Program.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static Pihrtsoft.Text.RegularExpressions.Linq.Patterns;
+
+namespace Pihrtsoft.Text.RegularExpressions.Linq
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Dump("c# quotation or comment", Snippets.CSharpLiteral());
+
+            Dump("cdata value", Snippets.XmlCData());
+
+            Dump("email address", Snippets.EmailAddress());
+
+            Dump("verbatim quoted text", Snippets.CSharpVerbatimTextLiteral());
+
+            Dump("leading whitespace", Snippets.LeadingWhiteSpace());
+
+            Dump("trailing whitespace", Snippets.TrailingWhiteSpace());
+
+            Dump("empty or whitespace line", Snippets.EmptyOrWhiteSpaceLine());
+
+            Dump("empty line", Snippets.EmptyLine());
+
+            Dump("first line without new line", Snippets.FirstLineWithoutNewLine());
+
+            Dump("linefeed without carriage return", Snippets.LinefeedWithoutCarriageReturn());
+
+            Dump("invalid file name chars", Any(Path.GetInvalidFileNameChars()));
+
+            Pattern exp = BeginInput()
+                .Assert(Crawl().SurroundWordBoundary("word1"))
+                .Assert(Crawl().SurroundWordBoundary("word2"))
+                .Any().MaybeMany();
+
+            Dump("äll words anywhere", exp);
+
+            var words = new string[] { "one", "two", "three" };
+
+            exp = WordBoundary()
+                .CountFrom(3,
+                    Any(words.Select(f => Group(Patterns.Text(f))))
+                    .WordBoundary()
+                    .NotWordChar().MaybeMany().Lazy())
+                .GroupReference(1)
+                .GroupReference(2)
+                .GroupReference(3);
+
+            Dump("words in sequence in any order", exp);
+
+            exp = Group(Word())
+                     .NotWordChars()
+                     .GroupReference(1)
+                     .WordBoundary();
+
+            Dump("repeated word", exp);
+
+            Console.ReadKey();
+        }
+
+        private static void Dump(Pattern pattern)
+        {
+            Dump(null, pattern);
+        }
+
+        private static void Dump(string title, Pattern pattern)
+        {
+            var options = PatternOptions.FormatAndComment;
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                Console.WriteLine($"{title}:");
+            }
+
+            Console.WriteLine(pattern.ToString(options));
+            Console.WriteLine(string.Empty);
+        }
+    }
+}
---
+++ Examples/Snippets.cs
@@ -0,0 +1,257 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using static Pihrtsoft.Text.RegularExpressions.Linq.Patterns;
+
+namespace Pihrtsoft.Text.RegularExpressions.Linq
+{
+    public static class Snippets
+    {
+        /// <summary>
+        /// Returns a pattern that matches a linefeed that is not preceded with a carriage return.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern LinefeedWithoutCarriageReturn()
+        {
+            return NoncapturingGroup(
+                NotAssertBack(CarriageReturn()).Linefeed());
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches leading whitespace of the string (or line if the <see cref="RegexOptions.Multiline"/> option is applied).
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern LeadingWhiteSpace()
+        {
+            return NoncapturingGroup(
+                BeginInputOrLine().WhiteSpaceExceptNewLine().OneMany());
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches trailing whitespace of the string (or line if the <see cref="RegexOptions.Multiline"/> option is applied). A carriage return is not included in the match.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern TrailingWhiteSpace()
+        {
+            return NoncapturingGroup(
+                WhiteSpaceExceptNewLine().OneMany()
+                .EndInputOrLine(true));
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches leading and trailing whitespace of the string (or line if the <see cref="RegexOptions.Multiline"/> option is applied). A carriage return is not included in the match.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern LeadingTrailingWhiteSpace()
+        {
+            return LeadingWhiteSpace() | TrailingWhiteSpace();
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that is empty or contains only whitespace(s). Neither carriage return nor linefeed is included in the match.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern EmptyOrWhiteSpaceLine()
+        {
+            return EmptyOrWhiteSpaceLine(false);
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that is empty or contains only whitespace(s), optionally including new line characters.
+        /// </summary>
+        /// <param name="includeNewLine">Indicates whether new line characters should be included in the match.</param>
+        /// <returns></returns>
+        public static QuantifiablePattern EmptyOrWhiteSpaceLine(bool includeNewLine)
+        {
+            if (includeNewLine)
+            {
+                return BeginLine()
+                    .WhileWhiteSpaceExceptNewLine()
+                    .Any(NewLine() | EndInput());
+            }
+            else
+            {
+                return BeginLine()
+                    .WhileWhiteSpaceExceptNewLine()
+                    .Assert(NewLine() | EndInput());
+            }
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches an empty line. Neither carriage return nor linefeed is included in the match.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern EmptyLine()
+        {
+            return EmptyLine(false);
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches an empty line, optionally including new line characters.
+        /// Empty line is defined as a start of line
+        /// </summary>
+        /// <param name="includeNewLine">Indicates whether new line characters should be included in the match.</param>
+        /// <returns></returns>
+        public static QuantifiablePattern EmptyLine(bool includeNewLine)
+        {
+            if (includeNewLine)
+            {
+                return BeginLine().NewLine();
+            }
+            else
+            {
+                return BeginLine().Assert(NewLine());
+            }
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that contains at least one non-whitespace character. New line characters are not included in the match.
+        /// </summary>
+        /// <returns></returns>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "NonWhite")]
+        public static QuantifiablePattern NonEmptyOrWhiteSpaceLine()
+        {
+            return NonEmptyOrWhiteSpaceLine(false);
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that contains at least one non-whitespace character, optionally including new line characters.
+        /// </summary>
+        /// <param name="includeNewLine">Indicates whether new line characters should be included in the match.</param>
+        /// <returns></returns>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "NonWhite")]
+        public static QuantifiablePattern NonEmptyOrWhiteSpaceLine(bool includeNewLine)
+        {
+            return NoncapturingGroup(
+                BeginLine()
+                    .WhileNotNewLineChar().Lazy()
+                    .NotWhiteSpace()
+                    .WhileNotNewLineChar()
+                    .AppendIf(includeNewLine, NewLine().Maybe()));
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that contains at least one character.  New line characters are not included in the match.
+        /// </summary>
+        /// <returns></returns>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "NonEmpty")]
+        public static QuantifiablePattern NonEmptyLine()
+        {
+            return NonEmptyLine(false);
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches a line that contains at least one character, optionally including new line characters.
+        /// </summary>
+        /// <param name="includeNewLine">Indicates whether new line characters should be included in the match.</param>
+        /// <returns></returns>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly", MessageId = "NonEmpty")]
+        public static QuantifiablePattern NonEmptyLine(bool includeNewLine)
+        {
+            return NoncapturingGroup(
+                BeginLine()
+                .NotNewLineChar().OneMany()
+                .AppendIf(includeNewLine, NewLine().Maybe()));
+        }
+
+        /// <summary>
+        /// Returns a pattern that matches first line of the string. Neither carriage return nor linefeed is included in the match.
+        /// </summary>
+        /// <returns></returns>
+        public static QuantifiablePattern FirstLineWithoutNewLine()
+        {
+            return NoncapturingGroup(
+                BeginInput().WhileNotNewLineChar());
+        }
+
+        public static Pattern CSharpLiteralOrComment()
+        {
+            return Any(
+                CSharpEscapedTextLiteral(),
+                CSharpVerbatimTextLiteral(),
+                CSharpCharacterLiteral(),
+                CSharpLineComment(),
+                CSharpMultilineComment());
+        }
+
+        public static Pattern CSharpLiteral()
+        {
+            return CSharpEscapedTextLiteral() | CSharpVerbatimTextLiteral() | CSharpCharacterLiteral();
+        }
+
+        public static Pattern CSharpTextLiteral()
+        {
+            return CSharpEscapedTextLiteral() | CSharpVerbatimTextLiteral();
+        }
+
+        public static Pattern CSharpEscapedTextLiteral()
+        {
+            var chars = MaybeMany(!Chars.QuoteMark().Backslash().NewLineChar());
+
+            return SurroundQuoteMarks(chars + MaybeMany(Backslash().NotNewLineChar() + chars));
+        }
+
+        public static Pattern CSharpVerbatimTextLiteral()
+        {
+            string q = "\"";
+
+            return "@" + q + WhileNotChar(q) + MaybeMany(q + q + WhileNotChar(q)) + q;
+        }
+
+        public static Pattern CSharpCharacterLiteral()
+        {
+            var chars = MaybeMany(!Chars.Apostrophe().Backslash().NewLineChar());
+
+            return SurroundApostrophes(chars + MaybeMany(Backslash().NotNewLineChar() + chars));
+        }
+
+        public static Pattern CSharpLineComment()
+        {
+            return "//" + NotNewLineChar().MaybeMany();
+        }
+
+        public static Pattern CSharpMultilineComment()
+        {
+            return "/*" 
+                + WhileNotChar('*')
+                + MaybeMany(
+                    "*" 
+                    + NotAssert("/") 
+                    + WhileNotChar('*'))
+                + "*/";
+        }
+
+        public static Pattern XmlCData()
+        {
+            return SurroundAngleBrackets(
+                    "!" + SurroundSquareBrackets(
+                        "CDATA" + SurroundSquareBrackets(
+                            Group(
+                                WhileNotChar(']')
+                                + MaybeMany(
+                                    "]"
+                                    + NotAssert("]>")
+                                    + WhileNotChar(']'))
+                            )
+                        )
+                    )
+                );
+        }
+
+        public static Pattern EmailAddress()
+        {
+            var left = OneMany(Chars.Alphanumeric() + "!#$%&'*+/=?^_`{|}~-");
+
+            var right = Maybe(MaybeMany(Chars.Alphanumeric() + "-").Alphanumeric());
+
+            return NoncapturingGroup(
+                left
+                + MaybeMany("." + left)
+                + "@"
+                + OneMany(Alphanumeric() + right + ".")
+                + Alphanumeric()
+                + right);
+        }
+    }
+}
