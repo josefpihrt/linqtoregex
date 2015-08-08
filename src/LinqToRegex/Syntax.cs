decafcf477353852f06e5aa938a894c9ff56596d
Josef Pihrt <josef.pihrt@outlook.com>
2015-08-08 11:46:17 +0200

    unnecessary usings removed

--- src/LinqToRegex/Syntax.cs
+++ src/LinqToRegex/Syntax.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.ObjectModel;
 using System.Text.RegularExpressions;
 
 namespace Pihrtsoft.Text.RegularExpressions.Linq
