d398487da716141763572975ebdef1bb1c699d82
Josef Pihrt <josef.pihrt@outlook.com>
2015-10-26 11:27:33 +0100

    SplitOptions enumeration added.

--- src/LinqToRegex/RegexSplit.cs
+++ src/LinqToRegex/RegexSplit.cs
@@ -76,6 +76,80 @@ namespace Pihrtsoft.Text.RegularExpressions.Linq
             yield return input.Substring(prevIndex, input.Length - prevIndex);
         }
 
+#if DEBUG
+        public static IEnumerable<string> EnumerateValues(Regex regex, string input, SplitOptions options)
+        {
+            return EnumerateValues(regex, input, 0, options);
+        }
+
+        public static IEnumerable<string> EnumerateValues(Regex regex, string input, int count, SplitOptions options)
+        {
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            return EnumerateValues(regex, input, count, regex.RightToLeft ? input.Length : 0, options);
+        }
+
+        public static IEnumerable<string> EnumerateValues(Regex regex, string input, int count, int startAt, SplitOptions options)
+        {
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (startAt < 0 || startAt > input.Length)
+                throw new ArgumentOutOfRangeException(nameof(startAt));
+
+            if (count == 1)
+            {
+                yield return input;
+                yield break;
+            }
+
+            Match firstMatch = regex.Match(input, startAt);
+            if (!firstMatch.Success)
+            {
+                yield return input;
+                yield break;
+            }
+
+            bool fIgnoreGroups = (options & SplitOptions.IgnoreGroups) != 0;
+            int prevIndex = 0;
+
+            foreach (var match in EnumerateMatches(firstMatch, count, regex.RightToLeft))
+            {
+                yield return input.Substring(prevIndex, match.Index - prevIndex);
+                prevIndex = match.Index + match.Length;
+
+                if (!fIgnoreGroups)
+                {
+                    if (regex.RightToLeft)
+                    {
+                        for (int i = (match.Groups.Count - 1); i >= 1; i--)
+                        {
+                            if (match.Groups[i].Success)
+                                yield return match.Groups[i].Value;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i < match.Groups.Count; i++)
+                        {
+                            if (match.Groups[i].Success)
+                                yield return match.Groups[i].Value;
+                        }
+                    }
+                }
+            }
+
+            yield return input.Substring(prevIndex, input.Length - prevIndex);
+        }
+#endif
+
         private static IEnumerable<Match> EnumerateMatches(Match match, int count, bool rightToLeft)
         {
             count--;
