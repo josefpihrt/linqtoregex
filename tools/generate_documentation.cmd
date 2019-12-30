88ab6632e638ddb691b82a7bc57a9addf4e52c1c
Josef Pihrt <josef.pihrt@outlook.com>
2019-12-30 14:31:34 +0100

    Add script for documentation generation

---
+++ tools/generate_documentation.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+"C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\LinqToRegex.sln" ^
+ /t:Clean,Build ^
+ /p:Configuration=Release,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591 ^
+ /v:normal ^
+ /fl ^
+ /m
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+set _roslynatorExe="..\..\roslynator\src\CommandLine\bin\Debug\net472\roslynator"
+set _msbuildPath="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+set _msbuildProperties="Configuration=Release"
+set _rootDirectoryUrl="../../docs/api/"
+
+%_msbuildPath%\msbuild "..\..\roslynator\src\CommandLine.sln" /t:Clean,Build /p:Configuration=Debug /v:m /m
+
+%_roslynatorExe% generate-doc "..\src\LinqToRegex.sln" ^
+ --msbuild-path %_msbuildPath% ^
+ --properties %_msbuildProperties% ^
+ --projects LinqToRegex ^
+ -o "..\docs\api" ^
+ -h "LINQ to Regex API Reference"
+
+%_roslynatorExe% list-symbols "..\src\LinqToRegex.sln" ^
+ --msbuild-path %_msbuildPath% ^
+ --properties %_msbuildProperties% ^
+ --projects LinqToRegex ^
+ --visibility public ^
+ --depth member ^
+ --ignored-parts containing-namespace assembly-attributes ^
+ --output "..\docs\api.txt"
+
+echo OK
+pause
+
